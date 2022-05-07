3fa5de0927339975998856a126aa10b53b715663
magics <hugo1.rodrigues@epitech.eu>
2022-05-08 01:54:40 +0200

    working on front

--- we-learn-front/src/metamask.jsx
+++ we-learn-front/src/metamask.jsx
@@ -28,7 +28,6 @@ async function checkIfWalletIsConnected(onConnected) {
   }
 }
 
-
 export default function MetaMaskAuth({ onAddressChanged }) {
   const [userAddress, setUserAddress] = useState("");
 
