18b093f9695b721e69154fff1d8f1fec0d209431
Mathis <mathis.lorenzo@epitech.eu>
2022-05-08 16:28:09 +0200

    add final quiz

--- we-learn-front/src/components/FormQuiz/FormQuiz.jsx
+++ we-learn-front/src/components/FormQuiz/FormQuiz.jsx
@@ -17,50 +17,60 @@ async function connect() {
 export default class FormQuiz extends React.Component {
   constructor(props) {
     super(props);
-    var tab = window.location.href.split('/');
     this.state = {
+      lessonInfos: [],
       response1: "",
       response2: "",
-      question1: "question 1",
-      question2: "question 2",
-      id: tab[tab.length - 1]
+      disable: true
     };
-    connect().then((r) => {
-      axios({
-        method: 'post',
-        url: API + '/get-formation',
-        headers: {}, 
-        data: {
-          wallet: r,
-          formation_id: this.state.id,
-        }
-      }).then((res) => {
-        console.log("aaaaa")
-        console.log(res)
+  }
+  
+  async componentDidMount() {
+    var tab = window.location.href.split('/');
+    var wallet = "";
+
+      connect().then((r) => {
+        wallet = r;
+        axios({
+          method: 'post',
+          url: API + '/get_formation',
+          headers: {}, 
+          data: {
+            wallet: wallet,
+            formation_id: tab[tab.length - 1],
+          }
+        }).then((res) => {
+          this.setState({ lessonInfos: res.data })
+        });
       });
-    });
   }
 
   handleChangeResponse1 = (e) => {
     this.setState({ response1: e.target.value });
+    if (this.state.response1 == this.state.lessonInfos.answer1 &&
+      this.state.response2 == this.state.lessonInfos.answer2)
+        this.setState({disable: false})
   };
   handleChangeResponse2 = (e) => {
     this.setState({ response2: e.target.value });
+    if (this.state.response1 == this.state.lessonInfos.answer1 &&
+      this.state.response2 == this.state.lessonInfos.answer2)
+        this.setState({disable: false})
   };
 
-  sendInfos = () => {
+  sendAnwser = () => {
     var wallet = "";
+    var tab = window.location.href.split('/');
+
     connect().then((r) => {
       wallet = r;
       axios({
         method: 'post',
-        url: API + '/create-formation',
+        url: API + '/finish_formation',
         headers: {}, 
         data: {
-          wallet: wallet,
-          formation_name: this.state.name,
-          answer1: this.state.response1,
-          answer2: this.state.response2,
+          buyer_wallet: wallet,
+          formation_id: tab[tab.length - 1],
         }
       });
     });
@@ -72,20 +82,20 @@ export default class FormQuiz extends React.Component {
       <h2>Final Quiz</h2>
       <form>
         <div class="user-box">
-          <input type="text" name="" required="" value={this.state.answer1} onChange={this.handleChangeResponse1}/>
-          <label>{this.state.question1}</label>
+          <input type="text" name="" required="" value={this.state.response1} onChange={this.handleChangeResponse1}/>
+          <label>{this.state.lessonInfos.question1}</label>
         </div>
         <div class="user-box">
-          <input type="text" name="" required="" value={this.state.answer2} onChange={this.handleChangeResponse2}/>
-          <label>{this.state.question2}</label>
+          <input type="text" name="" required="" value={this.state.response2} onChange={this.handleChangeResponse2}/>
+          <label>{this.state.lessonInfos.question2}</label>
         </div>
-          <div class="btn">
+          <button className="btn" onClick={this.sendAnwser} disabled={this.state.disable}>
             <span></span>
             <span></span>
             <span></span>
             <span></span>
-              Next
-          </div>
+              Submit
+          </button>
       </form>
       </div>
     );
