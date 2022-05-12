d5039450048c1a2192b4cd7c0286d869f87d60fb
magics <hugo1.rodrigues@epitech.eu>
2022-05-13 01:17:49 +0200

    make https api

--- WeLearn-api/server.js
+++ WeLearn-api/server.js
@@ -1,22 +1,19 @@
+const https = require("https");
 const express = require("express");
+
 const cors = require("cors");
-const app = express();
 const PORT = 8080;
 const routes = require("./routes/routes.js");
 const axios = require('axios').default;
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', routes);
 
-// app.post("/submit_quizz", async (req, res) => {
-//     formation_id = req.body.formation_id
-//     buyer_wallet = req.body.buyer_wallet
-
-//     res.send("quizz submitted")
-// });
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+https
+  .createServer(app)
+  .listen(4000, ()=>{
+    console.log('server is runing at port 4000')
 });
