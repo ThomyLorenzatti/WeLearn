39766a557b54addefe2e81a4af09da9a462c705a
magics <hugo1.rodrigues@epitech.eu>
2022-05-13 11:18:02 +0200

    working on backend

--- WeLearn-api/server.js
+++ WeLearn-api/server.js
@@ -5,18 +5,30 @@ const routes = require("./routes/routes.js");
 const app = express();
 var fs = require('fs');
 var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
-
-
-var credentials = {key: privateKey, cert: certificate};
+var certificate = fs.readFileSync('sslcert/server.cert', 'utf8');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', routes);
 
-https
-  .createServer(app, credentials)
-  .listen(4000, ()=>{
-    console.log('server is runing at port 4000')
-});
+https.createServer({
+  key: privateKey,
+  cert: certificate,
+  ca: certificateAuthority,
+  ciphers: [
+      "ECDHE-RSA-AES128-SHA256",
+      "DHE-RSA-AES128-SHA256",
+      "AES128-GCM-SHA256",
+      "RC4",
+      "HIGH",
+      "!MD5",
+      "!aNULL"].join(':'),
+  },
+app);
+
+// https
+//   .createServer(app, credentials)
+//   .listen(4000, ()=>{
+//     console.log('server is runing at port 4000')
+// });
