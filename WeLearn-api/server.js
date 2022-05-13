f41375d5226c858f4bbe049522511ea78141266b
magics <hugo1.rodrigues@epitech.eu>
2022-05-13 11:21:16 +0200

    update api

--- WeLearn-api/server.js
+++ WeLearn-api/server.js
@@ -12,23 +12,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', routes);
 
-https.createServer({
-  key: privateKey,
-  cert: certificate,
-  ca: certificateAuthority,
-  ciphers: [
-      "ECDHE-RSA-AES128-SHA256",
-      "DHE-RSA-AES128-SHA256",
-      "AES128-GCM-SHA256",
-      "RC4",
-      "HIGH",
-      "!MD5",
-      "!aNULL"].join(':'),
-  },
-app);
 
-// https
-//   .createServer(app, credentials)
-//   .listen(4000, ()=>{
-//     console.log('server is runing at port 4000')
-// });
+
+https
+  .createServer({
+  key: fs.readFileSync('./ssl/server.key'),
+  cert: fs.readFileSync('./ssl/server.cert')
+  }, app)
+  .listen(4000, ()=>{
+    console.log('server is runing at port 4000')
+});
