b9b2866e8b40ad4b0462ab4d69d5e57ce254a34b
magics <hugo1.rodrigues@epitech.eu>
2022-05-13 11:12:06 +0200

    added ssl certificats

--- WeLearn-api/server.js
+++ WeLearn-api/server.js
@@ -1,11 +1,14 @@
 const https = require("https");
 const express = require("express");
-
 const cors = require("cors");
-const PORT = 8080;
 const routes = require("./routes/routes.js");
-const axios = require('axios').default;
 const app = express();
+var fs = require('fs');
+var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+
+
+var credentials = {key: privateKey, cert: certificate};
 
 app.use(cors());
 app.use(express.json());
@@ -13,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', routes);
 
 https
-  .createServer(app)
+  .createServer(app, credentials)
   .listen(4000, ()=>{
     console.log('server is runing at port 4000')
 });
